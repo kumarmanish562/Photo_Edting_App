77f9a39413799339f70476726fbd976184bf2d82
kumarmanish562 <qmanishkumar562@gmail.com>
2025-03-04 05:15:59 +0530

    feat: Implement photo editing and camera integration with UI enhancements
    This commit introduces photo editing capabilities and camera integration to the application, along with UI enhancements.
    
    Key changes include:
    
    -   **`MainActivity.java`:**
        -   Implemented functionality to select images from the gallery or capture images using the camera.
        -   Added `onActivityResult` to handle image selection and camera capture results.
        -   Integrated the `DsPhotoEditor` SDK for photo editing features, allowing users to edit selected or captured images.
        -   Implemented logic to start the `ResultActivity` with the edited image.
        - Added permission check for the camera.
        - Added `getImageUri` method to convert bitmap to uri.
        - Integrated ads using `MobileAds`.
    -   **`AndroidManifest.xml`:**
        -   Added necessary permissions for internet access, external storage access and camera.
        - Declared `ResultActivity` and `DsPhotoEditorActivity`.
        - Added meta-data for ads and preloaded fonts.
    -   **`build.gradle` (Module :app):**
        - Added dependencies for `dsphotoeditor`, `glide`, `rxjava2`, `rxandroid` and `play-services-ads`.
        - Enabled `viewBinding`.
        - Configured compile options to use Java version 11.
    -   **`build.gradle` (Project):**
        - Updated the gradle version to 8.1.0.
    - **`settings.gradle`:**
        - Updated the repository mode to PREFER_SETTINGS.
    - **`ResultActivity.java`:**
        - Created this new activity to display the edited image.
    - Added `ds-photo-editor-sdk-v10.aar` lib.

--- app/src/main/java/com/manishkumar/mankieditz/ResultActivity.java
+++ app/src/main/java/com/manishkumar/mankieditz/ResultActivity.java
@@ -1,24 +1,45 @@
-package com.manishkumar.mankieditz;
-
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class ResultActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_result);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
+//package com.manishkumar.mankieditz;
+//
+//package com.mianasad.pico;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//
+//import com.google.android.gms.ads.AdListener;
+//import com.google.android.gms.ads.AdRequest;
+//import com.google.android.gms.ads.InterstitialAd;
+//import com.mianasad.pico.databinding.ActivityResultBinding;
+//
+//public class ResultActivity extends AppCompatActivity {
+//
+//    ActivityResultBinding binding;
+//    private InterstitialAd mInterstitialAd;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        binding = ActivityResultBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        getSupportActionBar().hide();
+//
+//        binding.image.setImageURI(getIntent().getData());
+//
+//        mInterstitialAd = new InterstitialAd(this);
+//        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+//        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+//
+//        mInterstitialAd.setAdListener(new AdListener() {
+//            @Override
+//            public void onAdLoaded() {
+//                super.onAdLoaded();
+//
+//                if(mInterstitialAd.isLoaded())
+//                    mInterstitialAd.show();
+//            }
+//        });
+//
+//    }
+//}
